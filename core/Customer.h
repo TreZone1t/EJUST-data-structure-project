5ef1cb3ce4889d099c846efb105e7fadbb20c108
tasbehamr-dev <tasbeehamr73@gmail.com>
2026-05-02 23:10:34 +0300

    Update Customer.h

--- core/Customer.h
+++ core/Customer.h
@@ -1,9 +1,39 @@
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
+class Customer {
+private:
+    int ticketNumber;
+    int arrivalTime;
+    int transactionTime;
+    int queueWaitTime;
+    int serviceEndTime;
+    int windowOpenTime;
+public:
+    Customer(int ticket, int arrival, int transaction) {
+        ticketNumber = ticket;
+        arrivalTime = arrival;
+        transactionTime = transaction;
+        queueWaitTime = 0;
+        serviceEndTime = 0;
+        windowOpenTime = 0;
+    }
+    Customer() {
+        ticketNumber = arrivalTime = transactionTime = 0;
+        queueWaitTime = serviceEndTime = windowOpenTime = 0;
+    }
 
-struct Customer {
-    int id;
-    int time;
+    int getTicketNumber()    { return ticketNumber; }
+    int getArrivalTime()     { return arrivalTime; }
+    int getTransactionTime() { return transactionTime; }
+    int getQueueWaitTime()   { return queueWaitTime; }
+    int getServiceEndTime()  { return serviceEndTime; }
+    int getWindowOpenTime()  { return windowOpenTime; }
+
+    void setQueueWaitTime(int t)  { queueWaitTime = t; }
+    void setServiceEndTime(int t) { serviceEndTime = t; }
+    void setWindowOpenTime(int t) { windowOpenTime = t; }
+
+    int getTotalTime() {
+        return serviceEndTime - arrivalTime;
+    }
 };
-
-#endif
