8c4761e7ebd72c87387f28e2ece42bb8bc2041f9
Yahya-Elghnaam <yahiaelghnaam684@gmail.com>
2026-05-05 17:14:20 +0200

    Implement server selection and run simulation
    Added server selection and simulation logic to manage customer arrivals and service times.

--- core/core.cpp
+++ core/core.cpp
@@ -6,6 +6,51 @@
 
 using namespace std;
 using namespace httplib;
+int free_server(vector<int>& serverEndTime) {
+    int idx = 0;
+    int earliest = serverEndTime[0];
+
+    for (int i = 1; i < serverEndTime.size(); i++) {
+        if (serverEndTime[i] < earliest) {
+            earliest = serverEndTime[i];
+            idx = i;
+        }
+    }
+    return idx;
+}
+void runSimulation(int Customers, int Servers) {
+    srand(time(0));
+    vector<int> arrival(Customers);
+    vector<int> service(Customers);
+    vector<int> serverEndTime(Servers, 0);
+    // Generate arrival times (inter-arrival)
+    arrival[0] = 0;
+    for (int i = 1; i < Customers; i++) {
+        arrival[i] = arrival[i - 1] + (rand() % 10 + 1);
+    }
+    // Generate service times
+    for (int i = 0; i < Customers; i++) {
+        service[i] = rand() % 5 + 1;
+    }
+    cout << "-------------------------------------------------------------------\n";
+    cout << "Arr\t \tWait\t \tStart\t \tServ\t \tEnd\t \tIdle\t Server\n";
+    cout << "--------------------------------------------------------------------\n";
+    for (int i = 0; i < Customers; i++) {
+        int server = free_server(serverEndTime);
+        int start = max(arrival[i], serverEndTime[server]);
+        int wait = start - arrival[i];
+        int idle = max(0, arrival[i] - serverEndTime[server]);
+        int end = start + service[i];
+        serverEndTime[server] = end;
+        cout << arrival[i] << "\t"<<"\t"
+             << wait << "\t"<<"\t"<<"\t"
+             << start << "\t" <<"\t" <<"\t"
+             << service[i] << "\t" <<"\t"<<"\t"
+             << end << "\t" <<"\t"<<"\t"
+             << idle<<"\t"<<"\t"
+             << server << endl;
+    }
+}
 
 int main()
 { 
