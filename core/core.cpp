0197835def8378ed73af73bb9fa5fa776f74aa71
TreZone1t <dlackview0@gmail.com>
2026-05-15 20:21:52 +0300

    finalize the project 2

--- core/core.cpp
+++ core/core.cpp
@@ -1,21 +1,6 @@
-#ifdef _WIN32
-#include <winsock2.h>
-#endif
 #include "ServerAPI.cpp"
-#include <cstdlib>
-#include <ctime>
-
 using namespace std;
-
 int main() {
-  srand(time(0));
-#ifdef _WIN32
-  WSADATA wsaData;
-  if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
-    cerr << "WSAStartup failed." << endl;
-    return 1;
-  }
-#endif
   try {
     ServerAPI *api = new ServerAPI(3);
     api->run();
